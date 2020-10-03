0e329d95fde23e49bbca433adebcb9f97eca4c15
kazuko.takasaki <zuu.cha26@gmail.com>
2020-10-03 16:03:49 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -24,13 +24,12 @@ class TasksController < ApplicationController
   end
 
   def edit
-    #
   end
 
   def update
-    #
     if @task.update(task_params)
       flash[:success] = 'タスクが更新されました'
+      redirect_to @task
     else
       flash.now[:danger] = 'タスクは更新されませんでした'
     　render :edit
@@ -44,12 +43,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
-  def set_task
+  def set_message
     @task = Task.find(params[:id])
   end
   
   private
    def task_params
-     params.require(:task).permit(:content)
+     params.require(:task).permit(:content, :status)
    end
 end
