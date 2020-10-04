5e532c15181dd8fe868b044d7b1d10505562ed93
kazuko.takasaki <zuu.cha26@gmail.com>
2020-10-04 14:17:40 +0000

    ログイン機能追加1004

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = current_user.tasks
   end
 
   def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -13,20 +15,23 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが登録されました'
       redirect_to @task
     else
       flash.now[:danger] = 'タスクが登録が失敗しました'
-    　render :new
+      render :new
     end
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
+     @task = current_user.tasks.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = 'タスクが更新されました'
       redirect_to @task
@@ -37,18 +42,23 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    #
+    @task = current_user.tasks.find(params[:id])
     @task.destroy
     flash[:success] = 'タスクは削除されました'
     redirect_to tasks_url
   end
   
-  def set_message
-    @task = Task.find(params[:id])
-  end
   
   private
+  
    def task_params
      params.require(:task).permit(:content, :status)
+   end
+   
+   def correct_user
+     @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+        redirect_to root_url
+        end 
    end
 end
